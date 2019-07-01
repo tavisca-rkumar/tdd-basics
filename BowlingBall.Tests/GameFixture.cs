a673e4d1ae50f3a336c4a090f6eee2e866a8fd8b
rkumar <rkumar@tavisca.in>
2019-07-01 21:13:00 +0530

    done

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -1,85 +1,14 @@
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BowlingBall;
+using Xunit;
 
 namespace BowlingBall.Tests
 {
-    [TestClass]
     public class GameFixture
     {
-        Game game = new Game();
-        private void RollMany(int n, int pins)
-        {
-            for (var i = 0; i < n; i++)
-                game.Roll(pins);
-        }
-
-        private void RollMany(int[] arr)
-        {
-            for (var i = 0; i < arr.Length; i++)
-                game.Roll(arr[i]);
-        }
-
-
-        [TestMethod]
-        public void TestZeroRoll()
-        {
-            RollMany(20, 0);
-            Assert.AreEqual(0, game.GetScore());
-        }
-
-        [TestMethod]
-        public void TestAllOnes()
-        {
-            RollMany(20, 1);
-            Assert.AreEqual(20, game.GetScore());
-        }
-
-        [TestMethod]
-        public void TestOneSpare()
-        {
-            game.Roll(6);
-            game.Roll(4);
-            game.Roll(3);
-            RollMany(17, 3);
-            Assert.AreEqual(67, game.GetScore());
-        }
-
-        [TestMethod]
-        public void TestOneStrike()
-        {
-            game.Roll(10);
-            game.Roll(3);
-            RollMany(17, 3);
-            Assert.AreEqual(70, game.GetScore());
-        }
-
-        [TestMethod]
-        public void TestAllSpare()
-        {
-            RollMany(new int[] { 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4 });
-            game.Roll(6);
-            Assert.AreEqual(160, game.GetScore());
-        }
-
-        [TestMethod]
-        public void TestAllStrike()
-        {
-            RollMany(10, 10);
-            game.Roll(10);
-            game.Roll(10);
-            Assert.AreEqual(300, game.GetScore());
-        }
-
-        [TestMethod]
-        public void TestWithNoSpareAndStrike()
+        [Fact]
+        public void DummyTest()
         {
             // This is a dummy test that will always pass.
-            RollMany(10, 4);
-            RollMany(10, 3);
-            Assert.AreEqual(70, game.GetScore());
         }
     }
 }
-
-
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,65 +4,16 @@ namespace BowlingBall
 {
     public class Game
     {
-        private int[] rollScore = new int[21];
-        private int[] frameScore = new int[10];
-        private int currentRoll = 0;
-
         public void Roll(int pins)
         {
-            rollScore[currentRoll] = pins;
-            currentRoll++;
+            throw new NotImplementedException();
         }
 
         public int GetScore()
         {
-            var totalScore = 0;
-            CalculateFrameScore(0);
-            for (var index = 0; index < 10; index++)
-            {
-                totalScore += frameScore[index];
-            }
-            return totalScore;
+            throw new NotImplementedException();
         }
 
-        public void CalculateFrameScore(int rollScoreIndex)
-        {
-            for (var index = 0; index < 10; index++)
-            {
-                if (IsStrike(rollScoreIndex))
-                {
-                    frameScore[index] = 10 + rollScore[rollScoreIndex + 1] + rollScore[rollScoreIndex + 2];
-                    rollScoreIndex += 1;
-                }
-                else if (IsSpare(rollScoreIndex))
-                {
-                    frameScore[index] = 10 + rollScore[rollScoreIndex + 2];
-                    rollScoreIndex += 2;
-                }
-                else
-                {
-                    frameScore[index] = rollScore[rollScoreIndex] + rollScore[rollScoreIndex + 1];
-                    rollScoreIndex += 2;
-                }
-            }
-        }
-
-        public bool IsSpare(int index)
-        {
-            if (rollScore[index] + rollScore[index + 1] == 10)
-                return true;
-            else
-                return false;
-        }
-
-        public bool IsStrike(int index)
-        {
-            ;
-            if (rollScore[index] == 10)
-                return true;
-            else
-                return false;
-
-        }
     }
 }
+
